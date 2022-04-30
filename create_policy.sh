caaed2a9f514085f3d120d849fff70691acf5e91
Erickrs2 <erickrs2esuarez@gmail.com>
2022-04-30 17:54:18 +0000

    c

--- create_policy.sh
+++ create_policy.sh
@@ -21,6 +21,21 @@ jq -n '{scripts: [$ARGS.named], type: "all"}' \
   --arg type "sig" \
   >> policy.script
 
+# Create policy script file (NFT)
+# echo "{" >> policy.script
+# echo "  \"type\": \"all\"," >> policy.script 
+# echo "  \"scripts\":" >> policy.script 
+# echo "  [" >> policy.script 
+# echo "   {" >> policy.script 
+# echo "     \"type\": \"before\"," >> policy.script 
+# echo "     \"slot\": $(expr $(cardano-cli query tip --mainnet | jq .slot?) + 10000)" >> policy.script
+# echo "   }," >> policy.script 
+# echo "   {" >> policy.script
+# echo "     \"type\": \"sig\"," >> policy.script 
+# echo "     \"keyHash\": \"$(cardano-cli address key-hash --payment-verification-key-file policy.vkey)\"" >> policy.script 
+# echo "   }" >> policy.script
+# echo "  ]" >> policy.script 
+# echo "}" >> policy.script
 
 # Create policy ID file
 echo $(cardano-cli transaction policyid --script-file policy.script) >> policy.id
